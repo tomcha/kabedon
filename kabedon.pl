2db636657cccd9ed515f34a8bff94f2d266bcc23
tomcha <tomcha@tomcha.net>
2016-01-23 22:49:44 +0900

    壁殴り通算回数を保持し、トップページに表示する

--- kabedon.pl
+++ kabedon.pl
@@ -1,14 +1,18 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
 
+my $cnt = 0;
+
 get '/' => sub {
   my $self = shift;
+  $self->stash(cnt => $cnt);
   $self->render(template => 'index');
 };
 
 get '/request' => sub {
   my $self = shift;
   my $dotsukitimes = $self->param('dotsukitimes');
+  $cnt += $dotsukitimes;
   $self->stash(dotsukitimes => $dotsukitimes);
   $self->render(template => 'request');
 };
@@ -21,6 +25,7 @@ __DATA__
 % layout 'default';
 % title '壁殴り代行サービス';
 <h1>壁殴り代行サービス</h1>
+<p>当サービスでは、通算<%= $cnt %>回の壁殴りを実施しました。</p>
 %= form_for '/request' => begin
   %= select_field dotsukitimes =>[['1回殴り' => 1], ['2回殴り' => 2], ['3回殴り' => 3]]
   %= submit_button '依頼する'
